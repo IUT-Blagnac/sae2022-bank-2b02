71170a90459277dfaaaa8c030b0caaa823f4774e
nicolaspoda <100203140+nicolaspoda@users.noreply.github.com>
2022-05-18 09:59:10 +0200

    Ajout de commentaires dans classe DailyBankState

--- src/application/DailyBankState.java
+++ src/application/DailyBankState.java
@@ -9,30 +9,56 @@ public class DailyBankState {
 	private AgenceBancaire agAct;
 	private boolean isChefDAgence;
 
+	/**
+	*Renvoie l'employé actif
+	*@return les informations liées à l'employé actif
+	**/
 	public Employe getEmpAct() {
 		return this.empAct;
 	}
 
+	/**
+	*Définit un employé actif
+	**/
 	public void setEmpAct(Employe employeActif) {
 		this.empAct = employeActif;
 	}
 
+	/**
+	*Renvoie l'agence bancaire active
+	*@return les informations liées à l'agence bancaire active
+	**/
 	public AgenceBancaire getAgAct() {
 		return this.agAct;
 	}
 
+	/**
+	*Définit une nouvelle agence bancaire active
+	**/
 	public void setAgAct(AgenceBancaire agenceActive) {
 		this.agAct = agenceActive;
 	}
 
+	/**
+	*Permet de connaitre le chef de l'agence bancaire
+	*@return true si la personne est chef d'agence, false sinon
+	**/
 	public boolean isChefDAgence() {
 		return this.isChefDAgence;
 	}
 
+	/**
+	*Définit un nouveau chef d'agence
+	*@param isChefDAgence : rôle du chef d'agence
+	**/
 	public void setChefDAgence(boolean isChefDAgence) {
 		this.isChefDAgence = isChefDAgence;
 	}
 
+	/**
+	*Ajoute les droits d'accès au chf d'agence
+	*@param droitAccess : droit d'accès du chef d'agence
+	**/
 	public void setChefDAgence(String droitsAccess) {
 		this.isChefDAgence = false;
 
